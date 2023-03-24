ec7d9e2f0816c8fdaa91a9c3808d64cbc37be9e3
chris <parweb@gmail.com>
2023-03-24 15:42:10 +0100

    up

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,26 @@
 import { App } from '@deepkit/app';
 import { FrameworkModule } from '@deepkit/framework';
-import { OpenAPIModule, Name } from 'deepkit-openapi';
-import { http, HttpBody, HttpError } from '@deepkit/http';
+import { OpenAPIModule } from 'deepkit-openapi';
 
-interface User {
-    id: number;
-    name: string;
-    password: string;
-}
-
-type CreateUser = Omit<User, 'id'>;
-type UpdateUser = Partial<CreateUser> & Name<'UpdateUser'>;
-type ReadUser = Omit<User, 'password'> & Name<'ReadUser'>;
-
-const users: User[] = [
-    { id: 1, name: 'chris', password: 'zekgjhzekg' },
-    { id: 2, name: 'cassi', password: 'zekgjhzekg' }
-];
-
-@http.controller()
-class UserController {
-    @http
-        .GET('/user/:id')
-        .response<ReadUser>(200, 'Read a User by its ID')
-        .response<Error>(404, 'User not found')
-    read(id: ReadUser['id']) {
-        const user = users.find(user => user.id === id);
-        if (!user) throw new HttpError('User not found', 404);
-        return user;
-    }
-
-    @http.POST('/user').response<ReadUser>(200, 'Create a new User')
-    create(user: HttpBody<CreateUser>) {
-        users.push({ id: users.length + 1, ...user });
-        return users.find(user => user.id === users.length - 1);
-    }
-
-    @http
-        .PATCH('/user/:id')
-        .response<ReadUser>(200, "Patch a User's attributes")
-        .response<Error>(404, 'User not found')
-    patch(id: number, patch: HttpBody<UpdateUser>) {
-        const user = users.find(user => user.id === id);
-        if (!user) throw new HttpError('User not found', 404);
-        return { ...user, ...patch };
-    }
-}
+import { UserController } from './user/controller';
 
 new App({
-    controllers: [UserController],
-    imports: [
-        new OpenAPIModule({
-            prefix: '/openapi/',
-            title: 'The title of your APIs',
-            description: 'The description of your APIs',
-            version: 'x.y.z'
-        }),
-        new FrameworkModule({
-            debug: true,
-            port: 4000,
-            publicDir: 'public',
-            httpLog: true,
-            migrateOnStartup: true
-        })
-    ]
+  controllers: [UserController],
+  imports: [
+    new OpenAPIModule({
+      prefix: '/openapi/',
+      title: 'The title of your APIs',
+      description: 'The description of your APIs',
+      version: 'x.y.z'
+    }),
+    new FrameworkModule({
+      debug: true,
+      port: 4000,
+      publicDir: 'public',
+      httpLog: true,
+      migrateOnStartup: true
+    })
+  ]
 })
-    .loadConfigFromEnv()
-    .run();
+  .loadConfigFromEnv()
+  .run();
---
+++ src/user/controller.ts
@@ -0,0 +1,48 @@
+import { http, HttpBody, HttpError } from '@deepkit/http';
+import { Name } from 'deepkit-openapi';
+
+interface User {
+  id: number;
+  name: string;
+  password: string;
+}
+
+type CreateUser = Omit<User, 'id'>;
+type UpdateUser = Partial<CreateUser> & Name<'UpdateUser'>;
+type ReadUser = Omit<User, 'password'> & Name<'ReadUser'>;
+
+const users: User[] = [
+  { id: 1, name: 'chris', password: 'zekgjhzekg' },
+  { id: 2, name: 'cassi', password: 'zekgjhzekg' }
+];
+
+@http.controller()
+export class UserController {
+  @http
+    .GET('/user/:id')
+    .name('get user')
+    .description('get one users')
+    .response<ReadUser>(200, 'Read a User by its ID')
+    .response<Error>(404, 'User not found')
+  read(id: ReadUser['id']) {
+    const user = users.find(user => user.id === id);
+    if (!user) throw new HttpError('User not found', 404);
+    return user;
+  }
+
+  @http.POST('/user').response<ReadUser>(200, 'Create a new User')
+  create(user: HttpBody<CreateUser>) {
+    users.push({ id: users.length + 1, ...user });
+    return users.find(user => user.id === users.length - 1);
+  }
+
+  @http
+    .PATCH('/user/:id')
+    .response<ReadUser>(200, "Patch a User's attributes")
+    .response<Error>(404, 'User not found')
+  patch(id: number, patch: HttpBody<UpdateUser>) {
+    const user = users.find(user => user.id === id);
+    if (!user) throw new HttpError('User not found', 404);
+    return { ...user, ...patch };
+  }
+}
